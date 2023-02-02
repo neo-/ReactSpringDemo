f573207d047a5e3b59a59615b296df4a820bdc23
rajeevan <naveejr@gmail.com>
2023-02-03 02:05:55 +0530

    Demo project for react js

---
+++ ReactTest/my-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Greet from './Components/Greet';
+import Welcome from './Components/Welcome';
+
+function App() {
+  return (
+    <div className="App">
+      <Greet />
+      <Welcome />
+    </div>
+  );
+}
+
+export default App;
