39dddf9b03630103463725a2f7d10b43199f6efc
rajeevan <naveejr@gmail.com>
2023-02-03 02:23:21 +0530

    Functional and Class

---
+++ ReactTest/my-app/src/Components/Hello.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hello = () => {
+    return (
+        <div id="hello" className="dummyClass">
+            <h1>Hello world!</h1>
+        </div>
+    )
+}
+
+export default Hello
