518d79eafe05217d8c9fff4173a3492f66beef2c
rajeevan <naveejr@gmail.com>
2023-02-02 23:44:33 +0530

    API for create, edit, delete, get , get list courses.

--- CourseServer/src/main/java/com/naveejr/courseserver/controller/CourseController.java
+++ CourseServer/src/main/java/com/naveejr/courseserver/controller/CourseController.java
@@ -3,11 +3,12 @@ package com.naveejr.courseserver.controller;
 import com.naveejr.courseserver.model.Course;
 import com.naveejr.courseserver.service.CourseService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,7 +18,49 @@ public class CourseController {
 	private final CourseService courseService;
 
 	@GetMapping("/courses")
-	public List<Course> getAllCourses() {
-		return courseService.findAllCourses();
+	public ResponseEntity<List<Course>> getAllCourses() {
+		return ResponseEntity.ok(courseService.findAllCourses());
 	}
+
+	@GetMapping("/courses/{id}")
+	public ResponseEntity<Course> getCourse(@PathVariable Long id) {
+		Optional<Course> optCourse = courseService.findCourse(id);
+		return optCourse.map(ResponseEntity::ok).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+	}
+
+	@PostMapping("/courses")
+	public ResponseEntity<Course> createCourse(@RequestBody Course course) {
+		try {
+			Course retCourse = courseService.createCourse(course);
+			return ResponseEntity.ok(retCourse);
+		} catch (Exception ex) {
+			return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
+		}
+	}
+
+	@PutMapping("/courses/{id}")
+	public ResponseEntity<Course> editCourse(@PathVariable Long id, @RequestBody Course course) {
+		Optional<Course> optCourse = courseService.findCourse(id);
+		if (optCourse.isPresent()) {
+			Course updateCourse = optCourse.get();
+			updateCourse.setCode(course.getCode());
+			updateCourse.setName(course.getName());
+			updateCourse.setDescription(course.getDescription());
+			updateCourse = courseService.editCourse(updateCourse);
+			return ResponseEntity.ok(updateCourse);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+
+	@DeleteMapping("/courses/{id}")
+	public ResponseEntity<HttpStatus> deleteCourse(@PathVariable Long id) {
+		try {
+			courseService.deleteCourse(id);
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		} catch (Exception ex) {
+			return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
+		}
+	}
+
 }
