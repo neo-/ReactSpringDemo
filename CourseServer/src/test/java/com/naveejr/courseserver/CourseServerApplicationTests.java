b6af98ccd983be3f11a1763b6b9a2e8a52bc2360
rajeevan <naveejr@gmail.com>
2023-02-02 23:13:10 +0530

    Server initial project for courses server

---
+++ CourseServer/src/test/java/com/naveejr/courseserver/CourseServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naveejr.courseserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
